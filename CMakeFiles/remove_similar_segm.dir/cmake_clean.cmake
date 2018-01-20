c42b09c5a49c65d9690727f261d47dbaa0d56d91
Paulo Abelha <pauloabelha@gmail.com>
2018-01-20 00:44:20 +0000

    Adding new files

---
+++ CMakeFiles/remove_similar_segm.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/remove_similar_segm.dir/remove_similar_segm.cpp.o"
+  "remove_similar_segm.pdb"
+  "remove_similar_segm"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/remove_similar_segm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/segment_meshes.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/segment_meshes.dir/segment_meshes.cpp.o"
+  "segment_meshes.pdb"
+  "segment_meshes"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/segment_meshes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
